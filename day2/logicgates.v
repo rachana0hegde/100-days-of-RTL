8d658bc3bfd0ee4fd6dff01c00cc783db8971506
rachana0hegde <rachanahegde.1@outlook.com>
2025-09-23 17:49:38 +0530

    day2-basic logic gates

--- day2/logicgates.v
+++ day2/logicgates.v
@@ -1,9 +1,6 @@
-'`timescale 1ps/1ps
 
-module logicgates(a,b,y_and,y_or,y_not);
-    input wire a,b;
-    output wire y_and,y_or,y_not;
 
+module logicgates(input a,b, output y_and, y_or, y_not);
     assign y_and = a & b;
     assign y_or  = a | b;
     assign y_not = ~a;
